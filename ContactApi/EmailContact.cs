11833394347eb961435c07d60d74d7f53735185d
William Applegate <wikapple@gmail.com>
2025-06-07 17:01:48 -0400

    Isolated model

--- ContactApi/EmailContact.cs
+++ ContactApi/EmailContact.cs
@@ -2,25 +2,30 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Functions.Worker;
 using Newtonsoft.Json;
 using Azure;
 using Azure.Communication.Email;
 using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
 
 namespace ContactApi
 {
-    public static class EmailContact
+    public class EmailContact
     {
-        [FunctionName("EmailContact")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        private readonly ILogger<EmailContact> _logger;
+        public EmailContact(ILogger<EmailContact> logger, IConfiguration config)
         {
-            log.LogInformation("Contact form submission received.");
+            _logger = logger;
+        }
+
+        [Function("EmailContact")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
+        {
+            _logger.LogInformation("Contact form submission received.");
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
